93e5d79d12f6f289b1428f5c069feee17ef9b109
schash1 <schash1@mail.ru>
2023-04-17 11:01:53 +0300

    Homework 2 Done

--- blog/app.py
+++ blog/app.py
@@ -1,8 +1,13 @@
-from flask import Flask
+from flask import Flask, render_template
+from blog.views.users import users_app
+from blog.views.articles import articles_app
+
 
 app = Flask(__name__)
+app.register_blueprint(users_app, url_prefix="/users")
+app.register_blueprint(articles_app, url_prefix="/articles")
 
 
 @app.route("/")
 def index():
-    return "Hello web!"
+    return render_template("index.html")
